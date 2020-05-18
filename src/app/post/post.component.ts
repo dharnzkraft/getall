14ab62c961d1c7999203eb903d25b2cefa19c50a
dharnihell <adeyinkadaniel09@gmail.com>
2020-05-18 21:14:49 +0100

    about to ionic

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -4,42 +4,67 @@ import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/storage';
 
+
 export class Item {
   body: string;
 }
+declare var $;
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
+  fabrics: Observable<any[]>;
   Posts: Observable<any[]>;
+  sales: Observable<any[]>;
+  elects: Observable<any[]>;
+  agric: Observable<any[]>;
+  auto: Observable<any[]>;
   Items: any;
   name: string;
-  category: string;
+  location: string;
   description: string;
   details: string;
   pic: any;
+  show = false;
 
   constructor(
     public db: AngularFireDatabaseModule,
     public afd: AngularFireDatabase,
     private storage: AngularFireStorage) {
     this.getDataFromFirebase();
-   }
+
+
+    }
 
   ngOnInit(): void {
   }
+  fabricShow() {
+    this.show = !this.show;
+  }
+  autoShow() {
+    this.show = !this.show;
+  }
+  electShow() {
+    this.show = !this.show;
+  }
+  agricShow() {
+    this.show = !this.show;
+  }
+  saleShow() {
+    this.show = !this.show;
+  }
 
-  addPost() {
-    this.afd.list(`Posts`).push({ names: this.name, category: this.category, desc: this.description, details: this.details,
+  addFabric() {
+    this.afd.list(`fabrics`).push({
+      fabricnames: this.name, fabriclocation: this.location, fabricdesc: this.description, fabricdetails: this.details,
        });
   }
 
   getDataFromFirebase() {
 
-    this.afd.list(`Posts`).valueChanges().subscribe(
+    this.afd.list(`fabrics`).valueChanges().subscribe(
       data => {
         console.log(data);
         this.Items = data;
@@ -53,4 +78,5 @@ export class PostComponent implements OnInit {
     const filePath = 'Posts';
     const task = this.storage.upload(filePath, file);
   }
+
 }
